56588dd94ce248dc74d5c03fe6ad73dd3facd084
Sean McGeer <basbalmanic@gmail.com>
2015-09-03 22:45:13 -0700

    Now with functions\!  Fixes #1.

--- rud.py
+++ rud.py
@@ -11,6 +11,92 @@ from urllib2 import urlopen, HTTPError
 
 user_agent = {'User-Agent': 'rud v0.1 by /u/manic0892 (github.com/Manic0892/rid)'}
 
+def imgur(i, user):
+	imgurURL = ''
+	imgurType = 0
+	#imgurType
+	#0 - Not Found
+	#1 - Straight-ahead image
+	#2 - .zip file
+	imgurID = ''
+	imgurURL = i['data']['url'].split('://')[1] #Strip out http(s) to avoid errors with http vs https
+	if i['data']['domain'] == 'i.imgur.com':
+		imgurType = 1
+		imgurID = re.search('(?<=i.imgur.com/)[A-Za-z0-9]+', imgurURL)
+		imgurID = imgurID.group(0)
+	elif i['data']['domain'] == 'm.imgur.com':
+		imgurType = 1
+		imgurID = re.search('(?<=m.imgur.com/)[A-Za-z0-9]+', imgurURL).group(0)
+		imgurURL = 'i.imgur.com/' + imgurID + '.png'
+	elif i['data']['domain'] == 'imgur.com':
+		m = re.search('(?<=imgur.com/a/)[A-Za-z0-9]+', imgurURL)
+		if m is None:
+			m = re.search('(?<=imgur.com/)[A-Za-z0-9]+', imgurURL)
+			if m is None:
+				m = re.search('(?<=imgur.com/gallery/)[A-Za-z0-9]+', imgurURL)
+				if m is not None:
+					imgurType = 1
+			else:
+				imgurType = 1
+		else:
+			imgurType = 2
+		
+		if m is not None:
+			imgurID = m.group(0)
+			if imgurType == 2:
+				imgurURL = 's.imgur.com/a/' + imgurID + '/zip'
+			elif imgurType == 1:
+				imgurURL = 'i.imgur.com/'+imgurID+'.png'
+			else:
+				imgurType = 0
+		else:
+			imgurType = 0
+
+	imgurURL = 'http://' + imgurURL #Un-strip http(s)
+	if imgurType == 1:
+		if not os.path.exists('./downloads/'+user+'/'+imgurID+'.png'):
+			print ('Downloading ' + imgurID + ' from ' + imgurURL + ' to ./downloads/'+user+'/'+imgurID+'.png')
+			urlretrieve(imgurURL, './downloads/'+user+'/'+imgurID+'.png')
+		else:
+			print ('Skipping previously downloaded image - ' + imgurID)
+	elif imgurType == 2:
+		if not os.path.exists('./downloads/'+user+'/'+imgurID+'.zip'):
+			print ('Downloading ' + imgurID + ' from ' + imgurURL + ' to ./downloads/'+user+'/'+imgurID+'.zip')
+			urlretrieve(imgurURL, './downloads/'+user+'/'+imgurID+'.zip')
+		else:
+			print ('Skipping previously downloaded album - ' + imgurID)
+	else:
+		print ('404 - ' + imgurURL)
+
+def gfycat(i, user):
+	gfycatURL = i['data']['url']
+	gfycatURL.replace('gfycat', 'giant.gfycat')
+	gfycatURL.strip('#')
+	gfycatID = re.search('(?<=gfycat.com/)[A-Za-z0-9]+', gfycatURL)
+	gfycatID = gfycatID.group(0)
+	gfycatURL = 'http://giant.gfycat.com/' + gfycatID + '.webm'
+	if os.path.exists('./downloads/'+user+'/'+gfycatID+'.webm'):
+		print ('Skipping previously downloaded gfy - ' + gfycatID)
+	else:
+		try:
+			urlopen(gfycatURL)
+		except HTTPError:
+			print('Error using giant link.  Switching to fat...')
+			try:
+				gfycatURL = 'http://fat.gfycat.com/' + gfycatID + '.webm'
+				urlopen(gfycatURL)
+			except HTTPError:
+				print ('Error using fat link.  Switching to zippy...')
+				try:
+					gfycatURL = 'http://zippy.gfycat.com/' + gfycatID + '.webm'
+					urlopen(gfycatURL)
+				except HTTPError:
+					print ('Tried to request ' + gfycatID + ' but encountered issues.  Downloading anyway using giant.')
+					gfycatURL = 'http://giant.gfycat.com/' + gfycatID + '.webm'
+
+		print ('Downloading ' + gfycatID + ' from ' + gfycatURL + ' to ./downloads/'+user+'/'+gfycatID+'.webm')
+		urlretrieve(gfycatURL, './downloads/'+user+'/'+gfycatID+'.webm')
+
 def getUsername(user):
 	print '- - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
 	#print '|'
@@ -36,90 +122,10 @@ def getUsername(user):
 			break
 		else:
 			for i in data['data']['children']:
-				imgurURL = ''
-				imgurType = 0
-				#imgurType
-				#0 - Not Found
-				#1 - Straight-ahead image
-				#2 - .zip file
-				imgurID = ''
 				if i['data']['domain'] == 'i.imgur.com' or i['data']['domain'] == 'imgur.com' or i['data']['domain'] == 'm.imgur.com':
-					imgurURL = i['data']['url'].split('://')[1] #Strip out http(s) to avoid errors with http vs https
-					if i['data']['domain'] == 'i.imgur.com':
-						imgurType = 1
-						imgurID = re.search('(?<=i.imgur.com/)[A-Za-z0-9]+', imgurURL)
-						imgurID = imgurID.group(0)
-					elif i['data']['domain'] == 'm.imgur.com':
-						imgurType = 1
-						imgurID = re.search('(?<=m.imgur.com/)[A-Za-z0-9]+', imgurURL).group(0)
-						imgurURL = 'i.imgur.com/' + imgurID + '.png'
-					elif i['data']['domain'] == 'imgur.com':
-						m = re.search('(?<=imgur.com/a/)[A-Za-z0-9]+', imgurURL)
-						if m is None:
-							m = re.search('(?<=imgur.com/)[A-Za-z0-9]+', imgurURL)
-							if m is None:
-								m = re.search('(?<=imgur.com/gallery/)[A-Za-z0-9]+', imgurURL)
-								if m is not None:
-									imgurType = 1
-							else:
-								imgurType = 1
-						else:
-							imgurType = 2
-						
-						if m is not None:
-							imgurID = m.group(0)
-							if imgurType == 2:
-								imgurURL = 's.imgur.com/a/' + imgurID + '/zip'
-							elif imgurType == 1:
-								imgurURL = 'i.imgur.com/'+imgurID+'.png'
-							else:
-								imgurType = 0
-						else:
-							imgurType = 0
-
-					imgurURL = 'http://' + imgurURL #Un-strip http(s)
-					if imgurType == 1:
-						if not os.path.exists('./downloads/'+user+'/'+imgurID+'.png'):
-							print ('Downloading ' + imgurID + ' from ' + imgurURL + ' to ./downloads/'+user+'/'+imgurID+'.png')
-							urlretrieve(imgurURL, './downloads/'+user+'/'+imgurID+'.png')
-						else:
-							print ('Skipping previously downloaded image - ' + imgurID)
-					elif imgurType == 2:
-						if not os.path.exists('./downloads/'+user+'/'+imgurID+'.zip'):
-							print ('Downloading ' + imgurID + ' from ' + imgurURL + ' to ./downloads/'+user+'/'+imgurID+'.zip')
-							urlretrieve(imgurURL, './downloads/'+user+'/'+imgurID+'.zip')
-						else:
-							print ('Skipping previously downloaded album - ' + imgurID)
-					else:
-						print ('404 - ' + imgurURL)
+					imgur(i, user)
 				elif i['data']['domain'] == 'gfycat.com':
-					gfycatURL = i['data']['url']
-					gfycatURL.replace('gfycat', 'giant.gfycat')
-					gfycatURL.strip('#')
-					gfycatID = re.search('(?<=gfycat.com/)[A-Za-z0-9]+', gfycatURL)
-					gfycatID = gfycatID.group(0)
-					gfycatURL = 'http://giant.gfycat.com/' + gfycatID + '.webm'
-					if os.path.exists('./downloads/'+user+'/'+gfycatID+'.webm'):
-						print ('Skipping previously downloaded gfy - ' + gfycatID)
-					else:
-						try:
-							urlopen(gfycatURL)
-						except HTTPError:
-							print('Error using giant link.  Switching to fat...')
-							try:
-								gfycatURL = 'http://fat.gfycat.com/' + gfycatID + '.webm'
-								urlopen(gfycatURL)
-							except HTTPError:
-								print ('Error using fat link.  Switching to zippy...')
-								try:
-									gfycatURL = 'http://zippy.gfycat.com/' + gfycatID + '.webm'
-									urlopen(gfycatURL)
-								except HTTPError:
-									print ('Tried to request ' + gfycatID + ' but encountered issues.  Downloading anyway using giant.')
-									gfycatURL = 'http://giant.gfycat.com/' + gfycatID + '.webm'
-
-						print ('Downloading ' + gfycatID + ' from ' + gfycatURL + ' to ./downloads/'+user+'/'+gfycatID+'.webm')
-						urlretrieve(gfycatURL, './downloads/'+user+'/'+gfycatID+'.webm')
+					gfycat(i, user)
 				else:
 					try:
 						f = open('./downloads/'+user+'/__URLS.txt','a')
